cd3be2cb4eaab0d9a8640ad61da71eb4c19e0eff
Jovan Conde <jovandconde@iskolarngbayan.pup.edu.ph>
2024-01-19 16:54:09 +0800

    sarrr

--- Project34/MyForm.h
+++ Project34/MyForm.h
@@ -46,17 +46,17 @@ namespace Project34 {
 
 	protected:
 
-	private: System::Windows::Forms::Label^ lblUser;
-	private: System::Windows::Forms::Label^ lblPass;
-	private: System::Windows::Forms::TextBox^ txtboxUser;
-	private: System::Windows::Forms::TextBox^ txtBoxPass;
 
 
 
 
-	private: System::Windows::Forms::Button^ btnSignin;
-	private: System::Windows::Forms::Label^ Signup;
-	private: System::Windows::Forms::Label^ label1;
+
+
+
+
+
+
+
 
 
 
@@ -66,13 +66,40 @@ namespace Project34 {
 
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Label^ label5;
-	private: System::Windows::Forms::Panel^ panel1;
 
 
-	private: System::Windows::Forms::Label^ label6;
+
+
 
 
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::TextBox^ txtBoxPass;
+	private: System::Windows::Forms::Label^ lblPass;
+	private: System::Windows::Forms::TextBox^ txtboxUser;
+	private: System::Windows::Forms::Label^ lblUser;
+	private: System::Windows::Forms::Button^ btnSignin;
+	private: System::Windows::Forms::Label^ Signup;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::PictureBox^ PBlogo;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox3;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::PictureBox^ pictureBox4;
+	private: System::Windows::Forms::Label^ label2;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -93,34 +120,67 @@ namespace Project34 {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->lblUser = (gcnew System::Windows::Forms::Label());
-			this->lblPass = (gcnew System::Windows::Forms::Label());
-			this->txtboxUser = (gcnew System::Windows::Forms::TextBox());
-			this->txtBoxPass = (gcnew System::Windows::Forms::TextBox());
-			this->btnSignin = (gcnew System::Windows::Forms::Button());
-			this->Signup = (gcnew System::Windows::Forms::Label());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			this->panel1->SuspendLayout();
+			this->txtBoxPass = (gcnew System::Windows::Forms::TextBox());
+			this->lblPass = (gcnew System::Windows::Forms::Label());
+			this->txtboxUser = (gcnew System::Windows::Forms::TextBox());
+			this->lblUser = (gcnew System::Windows::Forms::Label());
+			this->btnSignin = (gcnew System::Windows::Forms::Button());
+			this->Signup = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->PBlogo = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->PBlogo))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			this->panel2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// lblUser
+			// label4
 			// 
-			this->lblUser->BackColor = System::Drawing::Color::Transparent;
-			resources->ApplyResources(this->lblUser, L"lblUser");
-			this->lblUser->ForeColor = System::Drawing::SystemColors::Desktop;
-			this->lblUser->Name = L"lblUser";
+			this->label4->BackColor = System::Drawing::Color::Transparent;
+			this->label4->ForeColor = System::Drawing::SystemColors::Control;
+			resources->ApplyResources(this->label4, L"label4");
+			this->label4->Name = L"label4";
+			// 
+			// label5
+			// 
+			this->label5->BackColor = System::Drawing::Color::Transparent;
+			this->label5->ForeColor = System::Drawing::SystemColors::Control;
+			resources->ApplyResources(this->label5, L"label5");
+			this->label5->Name = L"label5";
+			// 
+			// pictureBox1
+			// 
+			resources->ApplyResources(this->pictureBox1, L"pictureBox1");
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->TabStop = false;
+			// 
+			// txtBoxPass
+			// 
+			this->txtBoxPass->BackColor = System::Drawing::SystemColors::Control;
+			this->txtBoxPass->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			resources->ApplyResources(this->txtBoxPass, L"txtBoxPass");
+			this->txtBoxPass->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->txtBoxPass->Name = L"txtBoxPass";
 			// 
 			// lblPass
 			// 
 			this->lblPass->BackColor = System::Drawing::Color::Transparent;
 			resources->ApplyResources(this->lblPass, L"lblPass");
-			this->lblPass->ForeColor = System::Drawing::SystemColors::Desktop;
+			this->lblPass->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(239)), static_cast<System::Int32>(static_cast<System::Byte>(200)),
+				static_cast<System::Int32>(static_cast<System::Byte>(162)));
 			this->lblPass->Name = L"lblPass";
 			// 
 			// txtboxUser
@@ -131,18 +191,18 @@ namespace Project34 {
 			this->txtboxUser->ForeColor = System::Drawing::SystemColors::ControlText;
 			this->txtboxUser->Name = L"txtboxUser";
 			// 
-			// txtBoxPass
+			// lblUser
 			// 
-			this->txtBoxPass->BackColor = System::Drawing::SystemColors::Control;
-			this->txtBoxPass->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			resources->ApplyResources(this->txtBoxPass, L"txtBoxPass");
-			this->txtBoxPass->ForeColor = System::Drawing::SystemColors::ControlText;
-			this->txtBoxPass->Name = L"txtBoxPass";
+			this->lblUser->BackColor = System::Drawing::Color::Transparent;
+			resources->ApplyResources(this->lblUser, L"lblUser");
+			this->lblUser->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(239)), static_cast<System::Int32>(static_cast<System::Byte>(200)),
+				static_cast<System::Int32>(static_cast<System::Byte>(162)));
+			this->lblUser->Name = L"lblUser";
 			// 
 			// btnSignin
 			// 
-			this->btnSignin->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(213)), static_cast<System::Int32>(static_cast<System::Byte>(206)),
-				static_cast<System::Int32>(static_cast<System::Byte>(163)));
+			this->btnSignin->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(164)),
+				static_cast<System::Int32>(static_cast<System::Byte>(83)));
 			this->btnSignin->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(213)),
 				static_cast<System::Int32>(static_cast<System::Byte>(206)), static_cast<System::Int32>(static_cast<System::Byte>(163)));
 			this->btnSignin->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(60)),
@@ -159,67 +219,91 @@ namespace Project34 {
 			// 
 			resources->ApplyResources(this->Signup, L"Signup");
 			this->Signup->BackColor = System::Drawing::Color::Transparent;
-			this->Signup->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->Signup->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(239)), static_cast<System::Int32>(static_cast<System::Byte>(200)),
+				static_cast<System::Int32>(static_cast<System::Byte>(162)));
 			this->Signup->Name = L"Signup";
 			this->Signup->Click += gcnew System::EventHandler(this, &MyForm::Signup_Click);
-			// 
-			// label1
-			// 
-			this->label1->BackColor = System::Drawing::Color::Transparent;
-			resources->ApplyResources(this->label1, L"label1");
-			this->label1->ForeColor = System::Drawing::SystemColors::Desktop;
-			this->label1->Name = L"label1";
-			// 
-			// label4
-			// 
-			this->label4->BackColor = System::Drawing::Color::Transparent;
-			this->label4->ForeColor = System::Drawing::SystemColors::Control;
-			resources->ApplyResources(this->label4, L"label4");
-			this->label4->Name = L"label4";
-			// 
-			// label5
-			// 
-			this->label5->BackColor = System::Drawing::Color::Transparent;
-			this->label5->ForeColor = System::Drawing::SystemColors::Control;
-			resources->ApplyResources(this->label5, L"label5");
-			this->label5->Name = L"label5";
-			// 
-			// panel1
-			// 
-			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(229)), static_cast<System::Int32>(static_cast<System::Byte>(229)),
-				static_cast<System::Int32>(static_cast<System::Byte>(203)));
-			this->panel1->Controls->Add(this->label6);
-			this->panel1->Controls->Add(this->label1);
-			this->panel1->Controls->Add(this->lblUser);
-			this->panel1->Controls->Add(this->txtboxUser);
-			this->panel1->Controls->Add(this->Signup);
-			this->panel1->Controls->Add(this->lblPass);
-			this->panel1->Controls->Add(this->btnSignin);
-			this->panel1->Controls->Add(this->txtBoxPass);
-			resources->ApplyResources(this->panel1, L"panel1");
-			this->panel1->Name = L"panel1";
 			// 
 			// label6
 			// 
 			resources->ApplyResources(this->label6, L"label6");
-			this->label6->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->label6->ForeColor = System::Drawing::SystemColors::Control;
 			this->label6->Name = L"label6";
 			// 
-			// pictureBox1
+			// panel1
 			// 
-			resources->ApplyResources(this->pictureBox1, L"pictureBox1");
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->TabStop = false;
-			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			resources->ApplyResources(this->panel1, L"panel1");
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Controls->Add(this->PBlogo);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Name = L"panel1";
+			// 
+			// label2
+			// 
+			resources->ApplyResources(this->label2, L"label2");
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(239)), static_cast<System::Int32>(static_cast<System::Byte>(200)),
+				static_cast<System::Int32>(static_cast<System::Byte>(162)));
+			this->label2->Name = L"label2";
+			// 
+			// PBlogo
+			// 
+			this->PBlogo->BackColor = System::Drawing::Color::Transparent;
+			resources->ApplyResources(this->PBlogo, L"PBlogo");
+			this->PBlogo->Name = L"PBlogo";
+			this->PBlogo->TabStop = false;
+			// 
+			// label1
+			// 
+			resources->ApplyResources(this->label1, L"label1");
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Name = L"label1";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->BackColor = System::Drawing::SystemColors::Control;
+			resources->ApplyResources(this->pictureBox2, L"pictureBox2");
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->BackColor = System::Drawing::SystemColors::Control;
+			resources->ApplyResources(this->pictureBox3, L"pictureBox3");
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->TabStop = false;
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->pictureBox4);
+			this->panel2->Controls->Add(this->pictureBox3);
+			this->panel2->Controls->Add(this->pictureBox2);
+			this->panel2->Controls->Add(this->panel1);
+			this->panel2->Controls->Add(this->label6);
+			this->panel2->Controls->Add(this->Signup);
+			this->panel2->Controls->Add(this->btnSignin);
+			this->panel2->Controls->Add(this->lblUser);
+			this->panel2->Controls->Add(this->txtboxUser);
+			this->panel2->Controls->Add(this->lblPass);
+			this->panel2->Controls->Add(this->txtBoxPass);
+			resources->ApplyResources(this->panel2, L"panel2");
+			this->panel2->Name = L"panel2";
+			// 
+			// pictureBox4
+			// 
+			resources->ApplyResources(this->pictureBox4, L"pictureBox4");
+			this->pictureBox4->Name = L"pictureBox4";
+			this->pictureBox4->TabStop = false;
 			// 
 			// MyForm
 			// 
 			this->AcceptButton = this->btnSignin;
 			resources->ApplyResources(this, L"$this");
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::ControlText;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(42)), static_cast<System::Int32>(static_cast<System::Byte>(14)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
+			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
 			this->ForeColor = System::Drawing::SystemColors::Control;
@@ -227,9 +311,15 @@ namespace Project34 {
 			this->HelpButton = true;
 			this->Name = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->PBlogo))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
 			this->ResumeLayout(false);
 
 		}
@@ -255,8 +345,8 @@ namespace Project34 {
 		Signup->ForeColor = System::Drawing::Color::Black;
 
 	}
-		private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
-	Application::Exit();
-	}
+	
+
+
 };
 }
--- Project34/OnBoarding.h
+++ Project34/OnBoarding.h
@@ -1,6 +1,12 @@
 #pragma once
 
+
+
+
+
 namespace Project34 {
+
+#include "OnBoarding1.h"
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -34,6 +40,12 @@ namespace Project34 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Button^ btnProceed;
+
 
 	protected:
 
@@ -51,20 +63,105 @@ namespace Project34 {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(OnBoarding::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->btnProceed = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Calibri", 27, System::Drawing::FontStyle::Bold));
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(239)), static_cast<System::Int32>(static_cast<System::Byte>(200)),
+				static_cast<System::Int32>(static_cast<System::Byte>(162)));
+			this->label1->Location = System::Drawing::Point(92, 134);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(245, 44);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Savor the Brew";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Calibri", 27, System::Drawing::FontStyle::Bold));
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(239)), static_cast<System::Int32>(static_cast<System::Byte>(200)),
+				static_cast<System::Int32>(static_cast<System::Byte>(162)));
+			this->label2->Location = System::Drawing::Point(66, 168);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(300, 44);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Embrace the Pause";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Calibri", 12));
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(93, 230);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(216, 38);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"The best grain, the finest roast, \r\n           the powerful flavor";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(189, 777);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(41, 11);
+			this->pictureBox1->TabIndex = 3;
+			this->pictureBox1->TabStop = false;
+			// 
+			// btnProceed
+			// 
+			this->btnProceed->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(164)),
+				static_cast<System::Int32>(static_cast<System::Byte>(83)));
+			this->btnProceed->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnProceed->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(42)), static_cast<System::Int32>(static_cast<System::Byte>(14)),
+				static_cast<System::Int32>(static_cast<System::Byte>(36)));
+			this->btnProceed->Location = System::Drawing::Point(97, 697);
+			this->btnProceed->Name = L"btnProceed";
+			this->btnProceed->Size = System::Drawing::Size(240, 40);
+			this->btnProceed->TabIndex = 4;
+			this->btnProceed->Text = L"Proceed";
+			this->btnProceed->UseVisualStyleBackColor = false;
+			this->btnProceed->Click += gcnew System::EventHandler(this, &OnBoarding::btnProceed_Click_1);
 			// 
 			// OnBoarding
 			// 
-			resources->ApplyResources(this, L"$this");
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(414, 820);
+			this->Controls->Add(this->btnProceed);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"OnBoarding";
-			this->Load += gcnew System::EventHandler(this, &OnBoarding::OnBoarding_Load);
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"OnBoarding";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void OnBoarding_Load(System::Object^ sender, System::EventArgs^ e) {
-	}
-	};
+	
+private: System::Void btnProceed_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	//Initializes the Menu
+	OnBoarding1^ onboarding1 = gcnew OnBoarding1();
+	//shows the menu CC
+	onboarding1->Show();
+}
+};
 }
